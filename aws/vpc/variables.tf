acde8bff6cba2408b8c3f259de8f81dbfea17d65
AJ Bond <aj@abond.dev>
2024-03-19 17:16:47 -0400

    feat(vpc): add tags variable

--- aws/vpc/variables.tf
+++ aws/vpc/variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "CIDR block of the virtual private cloud (VPC)"
 }
+
+variable "tags" {
+  type        = map(string)
+  description = "Map of strings that will be attached as tags to the resources"
+}
